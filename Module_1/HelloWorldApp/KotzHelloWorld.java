7f57f5d82bec9bde39dcd2f6ece905ada331cd27
cakotz <kotzcollin2020@gmail.com>
2024-03-11 11:46:35 -0500

    Create KotzHelloWorld.java

---
+++ Module_1/HelloWorldApp/KotzHelloWorld.java
@@ -0,0 +1,9 @@
+/*
+Written by Collin Kotz 2024
+*/
+class HelloWorldApp {
+    public static void main(String args[]) {
+        String output = "Hello World from Collin Kotz"
+        System.out.pringln(output);
+    }
+}

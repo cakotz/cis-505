31672bd9dba889315e877c82c8b3be28650be16c
cakotz <kotzcollin2020@gmail.com>
2024-03-12 18:25:30 -0500

    Hellow world app

--- Module_1/HelloWorldApp/KotzHelloWorld.java
+++
@@ -1,9 +0,0 @@
-/*
-Written by Collin Kotz 2024
-*/
-class HelloWorldApp {
-    public static void main(String args[]) {
-        String output = "Hello World from Collin Kotz"
-        System.out.pringln(output);
-    }
-}
---
+++ Module_1/HelloWorldApp/src/KotzHelloWorld.java
@@ -0,0 +1,6 @@
+public class KotzHelloWorld {
+    public static void main(String args[]) {
+        String output = "Hello World from Collin Kotz";
+        System.out.println(output);
+    }
+}
